ecfdc97033c36723ffd15178633a6d857855f98a
Gekctek <egcelletti@hey.com>
2022-04-20 09:11:37 -0700

    Test

--- test/Common.Tests/CandidTests.cs
+++ test/Common.Tests/CandidTests.cs
@@ -3,15 +3,17 @@ using Xunit;
 
 namespace Common.Tests
 {
-    public class CandidTests
-    {
-        [Theory]
-        [InlineData(1, "")]
-        public void Test1()
-        {
-            var nat = UnboundedUInt.FromUInt64(1);
-            var candidNat = CandidPrimitive.Nat(nat);
-            byte[] encodedValue = candidNat.EncodeValue();
-        }
-    }
+	public class CandidTests
+	{
+		[Theory]
+		[InlineData(1, "")]
+		public void Encode_Nat(ulong natValue, string expectedHex)
+		{
+			var nat = UnboundedUInt.FromUInt64(1);
+			var candidNat = CandidPrimitive.Nat(nat);
+			byte[] encodedValue = candidNat.EncodeValue();
+			string actualHex = HexUtil.BytesToHex(encodedValue);
+			Assert.Equal(expectedHex, actualHex);
+		}
+	}
 }
