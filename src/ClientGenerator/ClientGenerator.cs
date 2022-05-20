aae2162b5f4724659f069874bb8177b033cfbe95
Ethan Celletti <egcelletti@hey.com>
2022-05-20 12:20:28 -0700

    Generator tests

--- src/ClientGenerator/ClientGenerator.cs
+++ src/ClientGenerator/ClientGenerator.cs
@@ -1,5 +1,6 @@
-﻿//using ICP.Candid;
-//using ICP.Candid.Models.Types;
+﻿using ICP.Candid;
+using ICP.Candid.Models;
+using ICP.Candid.Models.Types;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Text;
 using System;
@@ -15,46 +16,62 @@ namespace ICP.ClientGenerator
     {
         public void Execute(GeneratorExecutionContext context)
         {
-            string source = "namespace PooPoo { public class A {} }";
-            context.AddSource("Testz", SourceText.From(source, Encoding.UTF8));
-            //IEnumerable<AdditionalText> didFiles = this.GetDidFiles(context);
-            //foreach (AdditionalText didFile in didFiles)
-            //{
-            //    this.GenerateClient(context, didFile);
-            //}
+            IEnumerable<AdditionalText> didFiles = this.GetDidFiles(context);
+            foreach (AdditionalText didFile in didFiles)
+            {
+                this.GenerateClient(context, didFile);
+            }
         }
 
         public void Initialize(GeneratorInitializationContext context)
         {
         }
 
-        //private void GenerateClient(GeneratorExecutionContext context, AdditionalText didFile)
-        //{
-        //    SourceText? sourceText = didFile.GetText();
-        //    if(sourceText == null)
-        //    {
-        //        return;
-        //    }
-        //    //string value = sourceText.ToString();
-        //    string name = Path.GetFileNameWithoutExtension(didFile.Path);
-        //    //CandidServiceType service = CandidTextParser.Parse<CandidServiceType>(value);
-        //    //string source = this.GenerateFromService(name, null);
-        //    string source = "namespace PooPoo { public class A {} }";
-        //    context.AddSource(name, source);
-        //}
+        private void GenerateClient(GeneratorExecutionContext context, AdditionalText didFile)
+        {
+            SourceText? sourceText = didFile.GetText();
+            if (sourceText == null)
+            {
+                return;
+            }
+            string value = sourceText.ToString();
+            string name = Path.GetFileNameWithoutExtension(didFile.Path);
+            CandidServiceType service;
+            try
+            {
+                //service = CandidTextParser.Parse<CandidServiceType>(value);
+                var methods = new Dictionary<string, CandidFuncType>
+                {
+                    {"one", new CandidFuncType(new List<FuncMode>(), new List<CandidType>(), new List<CandidType>()) }
+                };
+                service = new CandidServiceType(methods, CandidId.Parse("Governance"));
+            }
+            catch (Exception ex)
+            {
+                // TODO handle this better
+                DiagnosticDescriptor descriptor = new DiagnosticDescriptor("1", "Failed to parse DID file", ex.ToString(), "ParseError", DiagnosticSeverity.Error, true);
+                var location = Location.Create(didFile.Path, TextSpan.FromBounds(0, 0), new LinePositionSpan());
+                context.ReportDiagnostic(Diagnostic.Create(descriptor, location));
+                return;
+            }
+            string source = this.GenerateFromService(name, service);
+            context.AddSource(name, source);
+        }
 
-        //private string GenerateFromService(string name, CandidServiceType service)
-        //{
-        //    var stringBuilder = new IndentedStringBuilder();
-        //    stringBuilder.AppendLine($"public class {name}ApiClient");
-        //    stringBuilder.AppendLine("{");
-        //    //foreach ((string methodName, CandidFuncType func) in service.Methods)
-        //    //{
-        //    //    stringBuilder.
-        //    //}
-        //    stringBuilder.AppendLine("}");
-        //    return stringBuilder.ToString() ?? "";
-        //}
+        private string GenerateFromService(string name, CandidServiceType service)
+        {
+            var stringBuilder = new IndentedStringBuilder();
+            stringBuilder.AppendLine($"public class {name}ApiClient");
+            stringBuilder.AppendLine("{");
+            foreach ((string methodName, CandidFuncType func) in service.Methods)
+            {
+                stringBuilder.AppendLine($"public void {methodName}()");
+                stringBuilder.AppendLine("{");
+                stringBuilder.AppendLine("}");
+            }
+            stringBuilder.AppendLine("}");
+            return stringBuilder.ToString() ?? "";
+        }
 
         private IEnumerable<AdditionalText> GetDidFiles(GeneratorExecutionContext context)
         {
--- src/ClientGenerator/IndentedStringBuilder.cs
+++ src/ClientGenerator/IndentedStringBuilder.cs
@@ -8,14 +8,17 @@ namespace ICP.ClientGenerator
 {
     internal class IndentedStringBuilder
     {
+        private readonly StringBuilder stringBuilder = new StringBuilder();
+
         public void AppendLine(string value)
         {
-
+            this.stringBuilder.AppendLine(value);
         }
 
-        public IDisposable Indent()
+        public override string ToString()
         {
-            throw new NotImplementedException();
+            return this.stringBuilder.ToString();
         }
+
     }
 }
