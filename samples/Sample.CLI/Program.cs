6ef90aba658fa8ab148f33ac484f53f83b580370
Ethan Celletti <egcelletti@hey.com>
2022-11-27 16:27:01 -0800

    Removing temp files

--- samples/Sample.CLI/Program.cs
+++ samples/Sample.CLI/Program.cs
@@ -2,25 +2,25 @@ using EdjCase.ICP.Agent.Agents;
 using EdjCase.ICP.Agent.Auth;
 using EdjCase.ICP.Candid.Models;
 using Sample.Shared.Governance;
+using Path = EdjCase.ICP.Candid.Models.Path;
 
-//Uri url = new Uri($"https://ic0.app");
+Uri url = new Uri($"https://ic0.app");
 
-//var identity = new AnonymousIdentity();
-//IAgent agent = new HttpAgent(identity, url);
-
-
-//Principal canisterId = Principal.FromText("rrkah-fqaaa-aaaaa-aaaaq-cai");
-//var client = new GovernanceApiClient(agent, canisterId);
+var identity = new AnonymousIdentity();
+IAgent agent = new HttpAgent(identity, url);
 
 
-//var info = await client.GetProposalInfoAsync(62143, null);
-
-//var paths = new List<Path>
-//{
-//	Path.FromMultiString("time"),
-//	Path.FromMultiString("canister", "az5sd-cqaaa-aaaae-aaarq-cai", "module_hash"),
-//};
-//var a = await agent.ReadStateAsync(Principal.FromText("az5sd-cqaaa-aaaae-aaarq-cai"), paths);
+Principal canisterId = Principal.FromText("rrkah-fqaaa-aaaaa-aaaaq-cai");
+var client = new GovernanceApiClient(agent, canisterId);
 
 
-await new RSSOracle.RSS().Get();
+var info = await client.GetProposalInfoAsync(62143, null);
+
+var paths = new List<Path>
+{
+	Path.FromMultiString("time"),
+	Path.FromMultiString("canister", "az5sd-cqaaa-aaaae-aaarq-cai", "module_hash"),
+};
+var a = await agent.ReadStateAsync(Principal.FromText("az5sd-cqaaa-aaaae-aaarq-cai"), paths);
+
+
