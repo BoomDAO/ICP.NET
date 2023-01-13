130ab0cf7b0eaa23a8e61691530e014d1fb02c94
yuriy.toporovskyy@apocalypse333.com <yuriy.toporovskyy@apocalypse333.com>
2023-01-13 14:23:04 -0500

    Move project from pointless subdirectory up one level

--- publish.ps1
+++ publish.ps1
@@ -2,5 +2,5 @@ param(
     [string]$OutDir = "./dist"
 )
 
-dotnet publish src/InternetIdentity/EdjCase.ICP.InternetIdentity/EdjCase.ICP.InternetIdentity.csproj -o $OutDir -c Release /p:DebugType=None /p:DebugSymbols=false
+dotnet publish src/InternetIdentity/EdjCase.ICP.InternetIdentity.csproj -o $OutDir -c Release /p:DebugType=None /p:DebugSymbols=false
 dotnet publish src/Serialization/EdjCase.ICP.Serialization.csproj -o $OutDir -c Release /p:DebugType=None /p:DebugSymbols=false
