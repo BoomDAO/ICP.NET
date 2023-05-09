a7a1b787501df55e7202bd230688d94435a6d8af
Ethan Celletti <egcelletti@hey.com>
2023-05-08 21:13:07 -0700

    Cleanup

--- publish.ps1
+++
@@ -1,6 +0,0 @@
-param(
-    [string]$OutDir = "./dist"
-)
-
-dotnet publish src/InternetIdentity/EdjCase.ICP.InternetIdentity.csproj -o $OutDir -c Release /p:DebugType=None /p:DebugSymbols=false
-dotnet publish src/Serialization/EdjCase.ICP.Serialization.csproj -o $OutDir -c Release /p:DebugType=None /p:DebugSymbols=false
