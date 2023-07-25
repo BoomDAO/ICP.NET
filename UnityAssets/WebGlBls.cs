65c9d9a7d0ae77da05936dde6c70755a6df68b25
Ethan Celletti <egcelletti@hey.com>
2023-07-25 09:40:40 -0700

    Update WebGlBls.cs

--- UnityAssets/WebGlBls.cs
+++ UnityAssets/WebGlBls.cs
@@ -1,15 +1,10 @@
-using Cysharp.Threading.Tasks;
-using EdjCase.ICP.Agent.Agents.Http;
-using System;
-using System.Net;
-using System.Runtime.CompilerServices;
-using System.Threading;
-using System.Threading.Tasks;
-using UnityEngine;
-using UnityEngine.Networking;
 
-public class WebGlBls : IBlsCryptography
+#if UNITY_WEBGL
+public class WebGlBlsCryptography : IBlsCryptography
 {
+    public bool VerifySignature(byte[] publicKey, byte[] messageHash, byte[] signature)
+    {
+        return true;
+    }
 }
-
-
+#endif
