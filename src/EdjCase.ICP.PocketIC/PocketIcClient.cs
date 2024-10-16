4af2e59e6a5eb3aae439cf606997602bfc08f44b
Gekctek <Gekctek@Edjcase.com>
2024-10-16 12:34:10 -0700

    Removing lf enforcement

--- src/EdjCase.ICP.PocketIC/PocketIcClient.cs
+++ src/EdjCase.ICP.PocketIC/PocketIcClient.cs
@@ -5,6 +5,7 @@ using System.Text.Json;
 using System.Threading.Tasks;
 using System.Collections.Generic;
 using EdjCase.ICP.Candid.Models;
+using System.Net;
 
 namespace EdjCase.ICP.PocketIC
 {
@@ -13,36 +14,52 @@ namespace EdjCase.ICP.PocketIC
 		private readonly HttpClient httpClient;
 		private readonly string baseUrl;
 		private readonly TimeSpan processingTimeout;
+		private readonly int instanceId;
 		private bool isInstanceDeleted = false;
 
-		public PocketIcClient(string url, TimeSpan processingTimeout)
+		public PocketIcClient(HttpClient httpClient, string url, int instanceId, TimeSpan processingTimeout)
 		{
-			this.httpClient = new HttpClient();
+			this.httpClient = httpClient;
 			this.baseUrl = url;
+			this.instanceId = instanceId;
 			this.processingTimeout = processingTimeout;
 		}
 
-		public static async Task<PocketIcClient> CreateAsync(string url, TimeSpan? processingTimeout = null)
+		public static async Task<PocketIcClient> CreateAsync(
+			string url,
+			CreateInstanceRequest? request = null,
+			TimeSpan? processingTimeout = null)
 		{
 			var timeout = processingTimeout ?? TimeSpan.FromSeconds(30);
-			var client = new PocketIcClient(url, timeout);
 
-			// Here you might want to add any initialization logic, 
-			// such as creating an instance on the server
+			request ??= new CreateInstanceRequest();
+			var httpClient = new HttpClient();
+			var response = await PostAsync<CreateInstanceResponse>(httpClient, "/instances", request);
 
+			if (response.Error != null)
+			{
+				throw new Exception($"Failed to create PocketIC instance: {response.Error.Message}");
+			}
+
+			var client = new PocketIcClient(httpClient, url, response.Created!.InstanceId, timeout);
 			return client;
 		}
 
 		public async Task DeleteInstanceAsync()
 		{
 			this.AssertInstanceNotDeleted();
-			await this.httpClient.DeleteAsync($"{this.baseUrl}/instances");
+			await this.httpClient.DeleteAsync($"{this.baseUrl}/instances/{this.instanceId}");
 			this.isInstanceDeleted = true;
 		}
 
 		public async Task TickAsync()
 		{
 			await this.PostAsync<object>("/update/tick", null);
+		}
+
+		public async Task<Principal> GetPublicKeyAsync(GetPublicKeyRequest request)
+		{
+			return await this.PostAsync<byte[]>("/read/pub_key", request)
 		}
 
 		public async Task<ulong> GetTimeAsync()
@@ -59,13 +76,6 @@ namespace EdjCase.ICP.PocketIC
 			}
 			var request = new SetTimeRequest { NanosSinceEpoch = nanosSinceEpoch };
 			await this.PostAsync<object>("/update/set_time", request);
-		}
-
-		public async Task<Principal> CreateCanisterAsync(CreateCanisterOptions? options = null)
-		{
-			options ??= new CreateCanisterOptions();
-			var response = await this.PostAsync<CreateCanisterResponse>("/update/create_canister", options);
-			return response.CanisterId;
 		}
 
 		public async Task InstallCodeAsync(InstallCodeOptions options)
@@ -141,10 +151,15 @@ namespace EdjCase.ICP.PocketIC
 		private async Task<T> PostAsync<T>(string endpoint, object? data)
 		{
 			this.AssertInstanceNotDeleted();
+			string url = $"{this.baseUrl}{endpoint}";
+			return await PocketIcClient.PostAsync<T>(this.httpClient, url, data);
+		}
+
+		private static async Task<T> PostAsync<T>(HttpClient httpClient, string url, object? data)
+		{
 			var json = data != null ? JsonSerializer.Serialize(data) : "";
 			var content = new StringContent(json, Encoding.UTF8, "application/json");
-			Console.WriteLine($"{this.baseUrl}{endpoint}");
-			var response = await this.httpClient.PostAsync($"{this.baseUrl}{endpoint}", content);
+			var response = await httpClient.PostAsync(url, content);
 			response.EnsureSuccessStatusCode();
 			var responseContent = await response.Content.ReadAsStringAsync();
 			return JsonSerializer.Deserialize<T>(responseContent)!;
@@ -162,9 +177,70 @@ namespace EdjCase.ICP.PocketIC
 		{
 			this.httpClient.Dispose();
 		}
+
 	}
 
 	// Request and Response classes
+	public class CreateInstanceRequest
+	{
+		public CreateInstanceSubnetConfig SubnetConfigSet { get; set; }
+		public bool NonmainnetFeatures { get; set; }
+	}
+
+	public class CreateInstanceSubnetConfig
+	{
+		public SubnetConfig? Nns { get; set; }
+		public SubnetConfig? Sns { get; set; }
+		public SubnetConfig? Ii { get; set; }
+		public SubnetConfig? Fiduciary { get; set; }
+		public SubnetConfig? Bitcoin { get; set; }
+		public List<SubnetConfig> System { get; set; } = new List<SubnetConfig>();
+		public List<SubnetConfig> Application { get; set; } = new List<SubnetConfig>();
+		public List<SubnetConfig> VerifiedApplication { get; set; } = new List<SubnetConfig>();
+	}
+
+	public class SubnetConfig
+	{
+		public string DtsFlag { get; set; } // "Enabled" or "Disabled"
+		public string InstructionConfig { get; set; } // "Production" or "Benchmarking"
+		public object StateConfig { get; set; } // "New" or { "FromPath": [string, { "subnet_id": string }] }
+	}
+
+	public class CreateInstanceResponse
+	{
+		public CreatedInstance? Created { get; set; }
+		public ErrorResponse? Error { get; set; }
+	}
+
+	public class CreatedInstance
+	{
+		public int InstanceId { get; set; }
+		public Dictionary<string, SubnetTopology> Topology { get; set; }
+	}
+
+	public class SubnetTopology
+	{
+		public string SubnetKind { get; set; }
+		public int Size { get; set; }
+		public List<CanisterRange> CanisterRanges { get; set; }
+	}
+
+	public class CanisterRange
+	{
+		public CanisterId Start { get; set; }
+		public CanisterId End { get; set; }
+	}
+
+	public class CanisterId
+	{
+		public string CanisterIdValue { get; set; }
+	}
+
+	public class ErrorResponse
+	{
+		public string Message { get; set; }
+	}
+
 	public class GetTimeResponse
 	{
 		public ulong NanosSinceEpoch { get; set; }
@@ -173,18 +249,6 @@ namespace EdjCase.ICP.PocketIC
 	public class SetTimeRequest
 	{
 		public ulong NanosSinceEpoch { get; set; }
-	}
-
-	public class CreateCanisterOptions
-	{
-		public Principal? Sender { get; set; }
-		public ulong? Cycles { get; set; }
-		public List<Principal>? Controllers { get; set; }
-		public ulong? ComputeAllocation { get; set; }
-		public ulong? MemoryAllocation { get; set; }
-		public ulong? FreezingThreshold { get; set; }
-		public ulong? ReservedCyclesLimit { get; set; }
-		public Principal? TargetSubnetId { get; set; }
 	}
 
 	public class CreateCanisterResponse
